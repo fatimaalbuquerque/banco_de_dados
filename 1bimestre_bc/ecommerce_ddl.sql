ad7648a5500e0c44b5f3ca9a18a49dbf2391a2fc
Fatima Albuquerque <123821234+fatimaalbuquerque@users.noreply.github.com>
2024-03-28 10:56:15 -0300

    melhorando ecommerce_ddl.sql

--- 1bimestre_bc/ecommerce_ddl.sql
+++ 1bimestre_bc/ecommerce_ddl.sql
@@ -1,35 +1,46 @@
 create database ecommerce;
 use ecommerce;
 
+drop database ecommerce;
+drop table cliente;
+drop table produto;
+drop table pedido;
+drop table itens;
+
 create table cliente (
+	id_cliente int auto_increment primary key,
     nome varchar(100),
     endereco varchar(255),
     email varchar(255),
-    celular tinyint(10) unsigned,
+    celular varchar(30),
     dt_nascimento date
 );
 
 create table produto (
+	id_produto int auto_increment primary key,
     nome_produto varchar(100),
     preco decimal(5,2),
-    descricao varchar(500),
-    qtd_estoque int unsigned,
-    primary key (nome_produto, preco, descricao, qtd_estoque)
+    descricao text,
+    qtd_estoque int unsigned
 );
 
 create table pedido (
-	id_pedido bigint unsigned auto_increment,
+	id_pedido int auto_increment primary key,
+    id_cliente int,
     dt_compra date,
     valor_total decimal (6, 2),
     dt_entrega date,
-    primary key (id_pedido, valor_total)
+    foreign key (id_cliente) references cliente(id_cliente)
 );
 
 create table itens (
-    qtd_estoque bigint unsigned,
+	id_item int auto_increment primary key,
+    id_pedido int,
+    id_produto int,
+    qtd_estoque int unsigned,
     preco decimal(5, 2),
     valor_total decimal (6,2),
     
-    foreign key(preco, qtd_estoque) references produto(preco, qtd_estoque),
-    foreign key(valor_total) references pedido(valor_total)
+    foreign key(id_pedido) references pedido(id_pedido),
+    foreign key(id_produto) references produto(id_produto)
 );
